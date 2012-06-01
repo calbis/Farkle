e8b3634665dc6775116d5ed2dbd776d2f57dcdc8
Calvin Bishop <calbis@gmail.com>
2012-05-31 21:44:55 -0400

    Adding NumberOfDice GameRule

--- Farkle.Core/GameRules.cs
+++ Farkle.Core/GameRules.cs
@@ -7,9 +7,11 @@ namespace Farkle.Core
             // default rules
             WinningScore = 10000;
             ThresholdStartingScore = 500;
+            NuberOfDice = 5;
         }
 
         public int WinningScore { get; set; }
         public int ThresholdStartingScore { get; set; }
+        public int NuberOfDice { get; set; }
     }
 }
